4f7e7af07c4b9dea1380e8ad9f68b5840818b4c0
Marija Cehic <marijac@stuntcoders.com>
2022-03-02 17:59:26 +0100

    Add Header & Nav home mobile

--- dist/scripts/app.js
+++ dist/scripts/app.js
@@ -1,1 +1,1 @@
-"use strict";
+"use strict";jQuery,$(".icon--open").on("click",(function(){$(".nav__menu").toggleClass("show-menu"),$(this).addClass("display-none"),$(".icon--close").addClass("display-block"),$(".header__content").addClass("header__content--darken"),$(".header").addClass("header--darken")})),$(".icon--close").on("click",(function(){$(".nav__menu").toggleClass("show-menu"),$(".icon--open").removeClass("display-none"),$(this).removeClass("display-block"),$(".header__content").removeClass("header__content--darken"),$(".header").removeClass("header--darken")}));
