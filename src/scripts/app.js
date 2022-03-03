685aa0b18ae7852cc8d80e9b2c69f47b3a83cf89
Marija Cehic <marijac@stuntcoders.com>
2022-03-03 18:04:37 +0100

    Block nav menu page scrolling

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,17 +1,25 @@
 (function () {
+	//Menu open
 	$(".icon--open").on("click", function () {
 		$(".nav__menu").toggleClass("show-menu");
 		$(this).addClass("display-none");
 		$(".icon--close").addClass("display-block");
 		$(".header__content").addClass("header__content--darken");
 		$(".header").addClass("header--darken");
+
+		//Prevent  scroll when menu is open
+		$("body").addClass("body-menu-open");
 	});
 
+	//Menu close
 	$(".icon--close").on("click", function () {
 		$(".nav__menu").toggleClass("show-menu");
 		$(".icon--open").removeClass("display-none");
 		$(this).removeClass("display-block");
 		$(".header__content").removeClass("header__content--darken");
 		$(".header").removeClass("header--darken");
+
+		//Allow user to scroll when menu is closed again
+		$("body").removeClass("body-menu-open");
 	});
 })(jQuery);
